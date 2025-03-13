78f13ac7d30eb21018dd92b06503ca3dd4b22a06
Joao-H-Luz <joaohluz13@gmail.com>
2025-03-12 23:17:13 -0300

    Algoritimos de 2 a 5

--- algoritimo.java
+++ algoritimo.java
@@ -1,5 +1,6 @@
 public class algoritimo {
 
+    
 // Algoritimo 1°
     public int f1(int n){
         int res = 0;
@@ -14,5 +15,58 @@ public class algoritimo {
         return cont;
     }
 
-        
+
+// Algoritimo 2°
+    public int f2(int n){ 
+        int res = 0;
+        int cont = 0;
+        for(int i = n; i <= n+1; i += 2 ){
+            for(int j = n+1; j <= n*n; j += 2 ){
+                for(int k = i+1; k <= 2*n; k += 2 ){
+                    res = res + n*k;
+                    cont++;}
+                }
+            }
+        return cont;
+    }
+
+
+// // Algoritimo 3°
+//     public int f3(int n){ 
+//         int res = 0;
+//         int cont = 0;
+//         for(int i = n+1; i <= n+1; i += 1 ){
+//             for(int j = n; j <= i+1; j += j/2+1 ){
+//                 for(int k = j; k <= n*j; k += abs(j-i)+1 ){ 
+//                     res = res + n*k; 
+//                     cont++;}
+//                 }
+//             }
+//         return cont;
+//     }
+
+
+public int f4( int n ) { int i, j, k, res = 0 int cont_op = 0 for( i = n/2; i <= n*n; i += 1 ) for( j = 1; j <= n*n; j += i/2+1 ) for( k = 2; k <= i*j; k += j/2+1 ) { res = res + n-1 cont_op++ } return cont_op 8 }
+
+
+
+
+
+
+
+
+// Algoritimo 5°
+    public int f5(int n){
+        int res = 0;
+        int cont = 0;
+        for(int i = n/2; i <= n+1; i += 2 ){
+            for(int j = i; j <= n*i; j += i/2+1 ){
+                for(int k = i+1; k <= n*j; k += 1 ){
+                    res = res + k*j;
+                    cont++;}
+                }
+            }
+        return cont;
+    }
+
 }
--- app.java
+++ app.java
@@ -1,11 +1,11 @@
 public class app {
     public static void main(String[] args) {
-        algoritimo f  = new algoritimo(); //Cria o Objeto
-        
-        for(int n=10; n<1000; n+=50) {
-            int r = f.f1(n); // Chame uma a uma as funcoes 
-                             
-            System.out.println(n+";"+r); // analisa os seus comportamentos.
-        }     
+        algoritimo f = new algoritimo(); // Cria o Objeto
+
+        for (int n = 10; n < 1000; n += 50) {
+            int r = f.f4(n); // Chame uma a uma as funcoes
+
+            System.out.println(n + ";" + r); // analisa os seus comportamentos.
+        }
     }
 }
