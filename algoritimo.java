c1da1dfc3580a1eb54495a6a1dc99b1e15cb2c90
Joao-H-Luz <joaohluz13@gmail.com>
2025-03-13 16:53:57 -0300

    Resolvido o Problema do 3,4,5 algoritimos

--- algoritimo.java
+++ algoritimo.java
@@ -1,3 +1,4 @@
+import java.lang.Math;
 public class algoritimo {
 
     // Algoritimo 1°
@@ -30,28 +31,14 @@ public class algoritimo {
         return cont;
     }
 
-    // // Algoritimo 3°
-    // public int f3(int n){
-    // int res = 0;
-    // int cont = 0;
-    // for(int i = n+1; i <= n+1; i += 1 ){
-    // for(int j = n; j <= i+1; j += j/2+1 ){
-    // for(int k = j; k <= n*j; k += abs(j-i)+1 ){
-    // res = res + n*k;
-    // cont++;}
-    // }
-    // }
-    // return cont;
-    // }
-
-    // Algoritimo 4°
-    public int f4(int n) {
+    // Algoritimo 3°
+    public int f3(int n){
         int res = 0;
         int cont = 0;
-        for (int i = n / 2; i <= n * n; i += 1) {
-            for (int j = 1; j <= n * n; j += i / 2 + 1) {
-                for (int k = 2; k <= i * j; k += j / 2 + 1) {
-                    res = res + n - 1;
+        for(int i = n+1; i <= n+1; i += 1 ){
+            for(int j = n; j <= i+1; j += j/2+1 ){
+                for(int k = j; k <= n*j; k += Math.abs(j-i)+1 ){
+                    res = res + n*k;
                     cont++;
                 }
             }
@@ -59,19 +46,34 @@ public class algoritimo {
         return cont;
     }
 
-    // Algoritimo 5°
-    public int f5(int n) {
-        int res = 0;
+    // Algoritimo 4°
+    public long f4(long n) { // Ajustar a Variavel (n), para long
+        long res = 0;
         int cont = 0;
-        for (int i = n / 2; i <= n + 1; i += 2) {
-            for (int j = i; j <= n * i; j += i / 2 + 1) {
-                for (int k = i + 1; k <= n * j; k += 1) {
+        for (long i = n / 2; i <= n * n; i += 1) {
+            for (long j = 1; j <= n * n; j += i / 2 + 1) {
+                for (long k = 2; k <= i * j; k += j / 2 + 1) {
+                    res = res + n - 1;
+                    cont++;
+                }
+            }
+        }
+        return cont; // Evitar OverFLower
+    }
+
+    // Algoritimo 5°
+    public long f5(long n) { // Ajustar a Variavel (n), para long
+        long res = 0;
+        int cont = 0;
+        for (long i = n / 2; i <= n + 1; i += 2) {
+            for (long j = i; j <= n * i; j += i / 2 + 1) {
+                for (long k = i + 1; k <= n * j; k += 1) {
                     res = res + k * j;
                     cont++;
                 }
             }
         }
-        return cont;
+        return cont; // Evitar OverFLower
     }
 
 }
--- app.java
+++ app.java
@@ -2,8 +2,8 @@ public class app {
     public static void main(String[] args) {
         algoritimo f = new algoritimo(); // Cria o Objeto
 
-        for (int n = 10; n < 1000; n += 50) {
-            int r = f.f4(n); // Chame uma a uma as funcoes
+        for (long n = 10; n < 220; n += 30) {
+            long r = f.f5(n); // Chame uma a uma as funcoes
 
             System.out.println(n + ";" + r); // analisa os seus comportamentos.
         }
